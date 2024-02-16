45222e88825e8d25a8c5978cac5d61cb739ac91c
Brunogitpowa <92848695+Brunogitpowa@users.noreply.github.com>
2024-02-16 20:09:53 +0100

    Add files via upload

--- src/factorial.py
+++ src/factorial.py
@@ -8,7 +8,9 @@ def factorial(n):
     Calcula el factorial de un número.
     """
 
-    if n in {0, 1}:  # fusionar las comparaciones con 'in'
+    if not isinstance(n, int) or n < 0:
+        raise ValueError("El factorial no está definido para números no enteros o negativos.")
+    if n in {0, 1}:
         return 1
     return n * factorial(n - 1)
     
